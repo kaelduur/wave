4007303b53ca110f17f48ac4faa1cbe6ae83feab
Kıvanç Berk Ünver <kivancberkunver@gmail.com>
2019-05-31 20:46:08 +0300

    Update audio

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
